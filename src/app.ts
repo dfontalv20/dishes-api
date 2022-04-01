fed4c9c2f94cfc081dd5012e9921d27c21d031d7
Diego <d.andres2002@hotmail.com>
2022-04-01 17:23:17 -0500

    Añadido CORS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import * as database from "./database";
 import dishRoutes from "./routes/index";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 app.use('/dishes', dishRoutes);
 
